c3cd5f528261ef155ae229ec8b2f64c6c086881b
Kilian M <kilian02.markl@gmail.com>
2023-07-29 23:27:57 +0200

    [2020] refactor day 4

--- cpp20/src/day4.cpp
+++ cpp20/src/day4.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <sstream>
 #include <functional>
+#include <unordered_map>
+#include <iostream>
 
 #include "day4.h"
 #include "file_util.h"
@@ -12,21 +14,10 @@ Day4::~Day4() {};
 Passports Day4::read_input(std::string filename)
 {
     FileUtil fileutil;
-    std::vector<std::string> blocks = fileutil.read_split_double_newlines(filename);
+    std::string s = "abc";
+    std::vector<std::string> lines = fileutil.read_lines(filename);
+    lines.push_back("\n");
     Passports passports;
-
-    auto split_into_fields = [](std::string block) {
-        std::vector<std::string> fields;
-        std::istringstream line_stream(block);
-        std::string field, line;
-        while (std::getline(line_stream, line)) {
-            std::istringstream word_stream(line);
-            while (std::getline(word_stream, field, ' ')) {
-                fields.push_back(field);
-            }
-        }
-        return fields;
-    };
 
     auto split_into_pair = [](std::string key_val) { 
         std::size_t pos = key_val.find(':');
@@ -34,13 +25,27 @@ Passports Day4::read_input(std::string filename)
         return result;
     };
 
-    for (const auto& block : blocks) {
-        std::vector<std::string> fields = split_into_fields(block);
+    auto parse_passport = [&](std::vector<std::string> block) {
         Passport passport;
-        for (const auto& field : fields) {
-            passport.push_back(split_into_pair(field));
+        std::string field;
+        for (const auto& line : block) {
+            std::istringstream word_stream(line);
+            while (std::getline(word_stream, field, ' ')) {
+                passport.push_back(split_into_pair(field));
+            }
         }
-        passports.push_back(passport);
+        return passport;
+    };
+
+    auto block_begin = lines.begin();
+    decltype(block_begin) block_end;
+    for (auto it = lines.begin(); it != lines.end(); ++it) {
+        if ((*it).empty()) {
+            block_end = it;
+            std::vector<std::string> block(block_begin, block_end);
+            passports.push_back(parse_passport(block));
+            block_begin = block_end + 1;
+        }
     }
     return passports;
 }
@@ -73,52 +78,49 @@ std::string Day4::part2(std::string filename)
         }
     };
 
-    std::unordered_map<std::string, std::function<bool(const std::string&)>> check_funs = {
-        {"byr", [&](std::string byr_s) { return check_year(byr_s, 1920, 2002); }},
-        {"iyr", [&](std::string iyr_s) { return check_year(iyr_s, 2010, 2020); }},
-        {"eyr", [&](std::string eyr_s) { return check_year(eyr_s, 2020, 2030); }},
-        {"hgt", [ ](std::string hgt_s) {
-            std::size_t size;
-            try {
-                size = std::stoi(hgt_s.substr(0, hgt_s.size() - 2));
-            } catch (std::exception& e) {
-                return false;
-            }
-            std::string unit = hgt_s.substr(hgt_s.size() - 2);
-            bool is_cm = unit == "cm";
-            bool is_in = unit == "in";
-            return (is_cm && 150 <= size && size <= 193) || (is_in && 59 <= size && size <= 76);
-        }},
-        {"hcl", [ ](std::string hcl_s) {
-            if (hcl_s[0] != '#' || hcl_s.size() != 7) return false;
-            std::string hex = hcl_s.substr(1);
-            auto check_char = [](char c) { return ('0' <= c && c <= '9') || ('a' <= c && c <= 'f'); };
-            for (auto c : hex) {
-                if (!check_char(c)) {
-                    return false;
-                }
-            }
-            return true;
-        }},
-        {"ecl", [ ](std::string ecl_s) {
-            return (ecl_s == "amb" || ecl_s == "blu" || ecl_s == "brn" || 
-                ecl_s == "gry" || ecl_s == "grn" || ecl_s == "hzl" || ecl_s == "oth");
-        }},
-        {"pid", [ ](std::string pid_s) {
-            if (pid_s.size() != 9) return false;
-            for (auto c : pid_s) {
-                if (!('0' <= c && c <= '9')) return false;
-            }
-            return true;
-        }},
+    std::unordered_map<std::string, std::function<bool(const std::string&)>> check_funs;
+    check_funs["byr"] = [&](std::string byr_s) { return check_year(byr_s, 1920, 2002); };
+    check_funs["iyr"] = [&](std::string iyr_s) { return check_year(iyr_s, 2010, 2020); };
+    check_funs["eyr"] = [&](std::string eyr_s) { return check_year(eyr_s, 2020, 2030); };
+    check_funs["hgt"] = [ ](std::string hgt_s) {
+        std::size_t size;
+        try {
+            size = std::stoi(hgt_s.substr(0, hgt_s.size() - 2));
+        } catch (std::exception& e) {
+            return false;
+        }
+        std::string unit = hgt_s.substr(hgt_s.size() - 2);
+        bool is_cm = unit == "cm";
+        bool is_in = unit == "in";
+        return (is_cm && 150 <= size && size <= 193) || (is_in && 59 <= size && size <= 76);
+    };
+    check_funs["hcl"] = [](std::string hcl_s) {
+        if (hcl_s[0] != '#' || hcl_s.size() != 7) return false;
+        std::string hex = hcl_s.substr(1);
+        auto check_char = [](char c) { return std::isdigit(c) || ('a' <= c && c <= 'f'); };
+        for (auto c : hex) {
+            if (!check_char(c)) return false;
+        }
+        return true;
+    };
+    check_funs["ecl"] = [](std::string ecl_s) {
+        const std::vector<std::string> colors = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+        return std::find(colors.begin(), colors.end(), ecl_s) != colors.end();
+    };
+    check_funs["pid"] = [](std::string pid_s) {
+        if (pid_s.size() != 9) return false;
+        for (auto c : pid_s) {
+            if (!std::isdigit(c)) return false;
+        }
+        return true;
     };
 
     Passports passports = read_input(filename);
     int valid_count = 0;
-    for (auto& passport : passports) {
+    for (const auto& passport : passports) {
         int required = 0;
         bool valid = true;
-        for (auto& pair : passport) {
+        for (const auto& pair : passport) {
             if (pair.first == "cid") continue;
             ++required;
             if (!check_funs[pair.first](pair.second)) {
@@ -129,7 +131,6 @@ std::string Day4::part2(std::string filename)
         valid &= required == 7;
         if (valid) ++valid_count;
     }
-
 
     return std::to_string(valid_count);
 }

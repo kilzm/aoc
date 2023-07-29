fb0a483852f7942590d12a069e6ca9803a60f587
Kilian M <kilian02.markl@gmail.com>
2023-07-29 14:11:07 +0200

    [2020] improve day 2 perf

--- cpp20/src/day2.cpp
+++ cpp20/src/day2.cpp
@@ -1,8 +1,6 @@
 #include <vector>
 #include <string>
-#include <regex>
 #include <algorithm>
-#include <omp.h>
 
 #include "day2.h"
 #include "file_util.h"
@@ -16,18 +14,22 @@ std::vector<policy> Day2::read_input(std::string filename)
     FileUtil fileutil;
     std::vector<std::string> lines = fileutil.read_lines(filename);
     std::vector<policy> policies;
-    const std::regex pattern(R"((\d+)-(\d+) ([a-z]): ([a-z]+))");
+
+    auto parse_policy = [](std::string s) {
+        size_t pos_hyp = s.find('-');
+        size_t pos_spc = s.find(' ');
+        policy p {
+            .lower = std::stoi(s.substr(0, pos_hyp)),
+            .upper = std::stoi(s.substr(pos_hyp + 1, pos_spc)),
+            .character = s[pos_spc + 1],
+            .password = s.substr(pos_spc + 4),
+        };
+        return p;
+    };
+
     for (std::string line : lines) {
-        std::smatch matches;
-        if (std::regex_search(line, matches, pattern)) {
-            policy p {
-                .lower = std::stoi(matches[1]),
-                .upper = std::stoi(matches[2]),
-                .character = matches[3].str()[0],
-                .password = matches[4].str(),
-            };
-            policies.push_back(p);
-        }
+        policy p = parse_policy(line);
+        policies.push_back(p);
     }
     return policies;
 }
@@ -36,10 +38,7 @@ std::string Day2::part1(std::string filename)
 {
     std::vector<policy> policies = read_input(filename);
     int valid_count = 0;
-    omp_set_num_threads(2);
-    #pragma omp parallel for reduction(+:valid_count)
-    for (auto it = policies.begin(); it < policies.end(); ++it) {
-        auto p = *it;
+    for (const auto& p : policies) {
         int count = std::count(p.password.begin(), p.password.end(), p.character);
         if (p.lower <= count && count <= p.upper) {
             ++valid_count;
@@ -52,10 +51,7 @@ std::string Day2::part2(std::string filename)
 {
     std::vector<policy> policies = read_input(filename);
     int valid_count = 0;
-    omp_set_num_threads(2);
-    #pragma omp parallel for reduction(+:valid_count)
-    for (auto it = policies.begin(); it < policies.end(); ++it) {
-        auto p = *it;
+    for (const auto& p : policies) {
         if ((p.password[p.lower - 1] == p.character) != (p.password[p.upper - 1] == p.character)) {
             ++valid_count;
         }

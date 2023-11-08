41083e7c5decc94969d5365f694f65839cca5665
Kilian Markl <kilian02.markl@gmail.com>
2023-11-08 02:33:05 +0100

    Some housekeeping

--- cpp20/flake.nix
+++ cpp20/flake.nix
@@ -1,20 +1,21 @@
 {
-  description = "solutions to advent of code";
+  description = "solutions to advent of code 2020 in c++";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.cpp20.url = "path:./cpp20";
 
-  outputs = inputs @ { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       {
         packages = rec {
-          cpp20 = inputs.cpp20.packages.${system}.cpp20;
+          cpp20 = pkgs.callPackage ./. { stdenv = pkgs.gcc13Stdenv; };
+          default = cpp20;
         };
 
         apps = rec {
-          cpp20 = inputs.cpp20.apps.${system}.cpp20;
+          cpp20 = flake-utils.lib.mkApp { drv = self.packages.${system}.cpp20; };
+          default = cpp20;
         };
       }
     );
--- cpp20/src/days.cpp
+++ cpp20/src/days.cpp
@@ -14,6 +14,19 @@
 #include "day9.h"
 #include "day10.h"
 #include "day11.h"
+// #include "day12.h"
+// #include "day13.h"
+// #include "day14.h"
+// #include "day15.h"
+// #include "day16.h"
+// #include "day17.h"
+// #include "day18.h"
+// #include "day19.h"
+// #include "day20.h"
+// #include "day21.h"
+// #include "day22.h"
+// #include "day23.h"
+// #include "day24.h"
 
 Days::Days()
 {
@@ -28,6 +41,19 @@ Days::Days()
     m_days[8] = new Day9();
     m_days[9] = new Day10();
     m_days[10] = new Day11();
+    // m_days[11] = new Day12();
+    // m_days[12] = new Day13();
+    // m_days[13] = new Day14();
+    // m_days[14] = new Day15();
+    // m_days[15] = new Day16();
+    // m_days[16] = new Day17();
+    // m_days[17] = new Day18();
+    // m_days[18] = new Day19();
+    // m_days[19] = new Day20();
+    // m_days[20] = new Day21();
+    // m_days[21] = new Day22();
+    // m_days[22] = new Day23();
+    // m_days[23] = new Day24();
 }
 
 Days::~Days()

36399c448f8c934d0e0b6a12bfe184eb611cdf31
Kilian Markl <kilian02.markl@gmail.com>
2024-03-01 01:53:01 +0100

    Add aoc2023 to main flake

--- flake.nix
+++ flake.nix
@@ -4,17 +4,19 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.cpp20.url = "path:./cpp20";
+  inputs.odin23.url = "path:./odin23";
 
   outputs = inputs @ { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
       {
-        packages = rec {
-          cpp20 = inputs.cpp20.packages.${system}.cpp20;
+        packages = {
+          cpp20 = inputs.cpp20.packages.${system}.default;
+          odin23 = inputs.odin23.packages.${system}.default;
         };
 
-        apps = rec {
-          cpp20 = inputs.cpp20.apps.${system}.cpp20;
+        apps = {
+          cpp20 = inputs.cpp20.apps.${system}.default;
+          odin23 = inputs.odin23.apps.${system}.default;
         };
       }
     );

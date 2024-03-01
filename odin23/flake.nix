20ebe6eb7080a4340e735dbf7857358e975bae13
Kilian Markl <kilian02.markl@gmail.com>
2024-03-01 01:51:21 +0100

    Use odin overlay

--- odin23/flake.nix
+++ odin23/flake.nix
@@ -3,50 +3,35 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.odin-overlay.url = "github:kilzm/odin-overlay";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    let
-      overlays = [
-        (final: prev: {
-          odin = (prev.odin.overrideAttrs(old: {
-            version = "034aead9301305d41756ef3b5b9b60a88c95d825";
-            src = old.src.override {
-              rev = "034aead9301305d41756ef3b5b9b60a88c95d825";
-              hash = "sha256-okfcOlajq+r3oHH9zRHqaND4kIq3LWKYfEK7WTaI8hk=";
-            };
-          })).override {
-            llvmPackages_13 = prev.llvmPackages_17;
-          };
-         })
-      ];
-    in
+  outputs = { self, nixpkgs, flake-utils, odin-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let 
         pkgs = import nixpkgs {
-          inherit system overlays; 
-        };
-        pkgs2 = import nixpkgs {
           inherit system;
         };
+        odin-pkgs = odin-overlay.packages.${system};
       in
       {
         devShells = {
           default = pkgs.mkShell {
-            packages = with pkgs; [
-              odin
+            packages = with odin-pkgs; [
+              odin-latest
+              ols
             ];
           };
         };
 
         packages = rec {
-          odin23 = pkgs.callPackage ./. { stdenv = pkgs.gcc13Stdenv; };
-          odin23-slow = pkgs2.callPackage ./. { odin = pkgs2.odin.override { llvmPackages_13 = pkgs2.llvmPackages_17; }; };
-          default = odin23;
+          odin23 = pkgs.callPackage ./. { odin = odin-pkgs.odin-latest; };
+          odin23-fast = pkgs.callPackage ./. { odin = odin-pkgs.odin-sroa-pass; };
+          default = odin23-fast;
         };
 
         apps = rec {
-          odin23 = flake-utils.lib.mkApp { drv = self.packages.${system}.odin23; };
-          default = odin23;
+          odin23-fast = flake-utils.lib.mkApp { drv = self.packages.${system}.odin23-fast; };
+          default = odin23-fast;
         };
       }
     );
